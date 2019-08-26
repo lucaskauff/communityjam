ddcb785e3ad141fdf4573069f511e3c28f5def17
Lucas Kauff <l.kauff@rubika-edu.com>
2019-08-27 00:11:37 +0200

    tic toc the clock

---
+++ Barmie the barmy barman/Assets/1_Scripts/UI_Scripts/ClockFunctionning.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockFunctionning : MonoBehaviour
+{
+    //Serializable
+    [Header("My components")]
+    [SerializeField] Animator myAnim = default;
+
+    [Header("Gameobjects to serialize")]
+    [SerializeField] GameObject daysPanel = default;
+    [SerializeField] GameObject hoursPanel = default;
+
+    //Private
+    bool isOpened = false;
+
+    private void Update()
+    {
+        daysPanel.SetActive(isOpened);
+        hoursPanel.SetActive(isOpened);
+    }
+
+    public void HasBeenClickedOn()
+    {
+        isOpened = !isOpened;
+        myAnim.SetBool("IsOpened", isOpened);
+    }
+}
